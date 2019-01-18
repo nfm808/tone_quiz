289fc74d15764594ae79e695b0cdff8499d86db7
Nathan Fowler <luxurysandbox@gmail.com>
2019-01-17 19:23:07 -0500

    added bare bones html for testing of functions

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function renderQuiz() {
+    // render the quiz
+    console.log('`renderQuiz` ran');
+
+    // insert HTML into the DOM
+};
+
+// add to the score
+function scorePlusOne() {
+    console.log('`scorePlusOne` ran');
+};
+
+// resets the quiz to beginning
+function resetQuiz() {
+    console.log('`resetQuiz` ran');
+};
+
+// play tone with speaker icon
+function playQuestionTone() {
+    console.log('`playTone` ran');
+};
+
+// play tone associated with answer selection
+function playAnswerTone() {
+    console.log('`playAnswerTone` ran');
+};
+
+// when the submit button is hit
+function handleSubmit() {
+    console.log('`handleSubmit` ran')
+};
+
